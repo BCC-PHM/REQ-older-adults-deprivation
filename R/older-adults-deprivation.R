7038af3f1c2714f4407ac6e55102b82468f3d03b
David.Ellis <david.ellis@birmingham.gov.uk>
2024-11-12 09:17:58 +0000

    Added deprivation metric = scaled IDAOPI * 65+ population

--- R/older-adults-deprivation.R
+++ R/older-adults-deprivation.R
@@ -45,5 +45,54 @@ map
 # Save map
 save_map(map, "../output/Birmingham-IDAOPI.png")
 
-###
-  
+############################################################
+#           Metric = IDAOPI * (65+ Population)             #
+############################################################
+
+birmingham_lsoa11_to_lsoa21 <- read.csv("../data/LSOA11_to_LSOA21_to_LA_Lookup.csv") %>%
+  filter(LAD22NM == "Birmingham") %>%
+  mutate(
+    LSOA11 = LSOA11CD, 
+    LSOA21 = LSOA21CD) %>%
+  select(LSOA11, LSOA21)
+
+oa_pop <- readxl::read_excel("../data/census-brum-oa-lsoa.xlsx") %>%
+  # Restrict to 65+
+  filter(`Age (3 categories)` == "Aged 65 years and over") %>%
+  # Update column names
+  mutate(LSOA21 = `Lower layer Super Output Areas Code`) %>%
+  select(LSOA21, Observation) %>% 
+  # Join LSOA11 to LSOA21 lookup
+  left_join(
+    birmingham_lsoa11_to_lsoa21,
+    by = join_by(LSOA21)
+    ) 
+metric <- oa_pop %>%
+  # Calculate Population for each LSOA11
+  group_by(LSOA11) %>%
+  summarise(Observation = sum(Observation)) %>%
+  # Join Birmingham IDAOPI
+  left_join(
+    Brum_IDAOPI,
+    by = join_by(LSOA11)
+  ) %>%
+  mutate(
+    OA_dep_metric = Brum_IDAOPI_rank_perc * Observation,
+    OA_dep_metric_norm = OA_dep_metric / max(OA_dep_metric)
+  )
+
+# Plot map
+metric_map <- plot_map(
+  metric,
+  "OA_dep_metric_norm",
+  "LSOA11",
+  area_name = "Birmingham",
+  style = "cont",
+  map_title = "Birmingham Older Adults Deprivation Metric (IDAOPI * 65+ Population)",
+)
+
+# Show map
+metric_map
+
+# Save map
+save_map(metric_map, "../output/Birmingham-IDAOPI-metric.png")
